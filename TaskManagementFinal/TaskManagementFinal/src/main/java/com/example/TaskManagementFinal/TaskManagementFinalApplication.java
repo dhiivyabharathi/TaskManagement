e9fe8f21baef09d38da53fa30b9c0b4df1f432cd
Dhivya Bharathi <145977800+dhiivyabharathi@users.noreply.github.com>
2024-04-05 22:53:34 +0530

    Add files via upload

---
+++ TaskManagementFinal/TaskManagementFinal/src/main/java/com/example/TaskManagementFinal/TaskManagementFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagementFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementFinalApplication.class, args);
+	}
+
+}
